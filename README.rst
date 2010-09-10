0bd02f4f4a2fef4d1c6eabdd625316823c92650f
Eric Holscher <eric@ericholscher.com>
2010-09-10 16:37:12 -0500

    Added basic readme and setup.py

---
+++ README.rst
@@ -0,0 +1,2 @@
+A crawler using the Django Test Client.
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = "django-crawler",
+    version = "0.1",
+    packages = [
+        "crawler",
+        "crawler.management",
+        "crawler.management.commands",
+    ],
+    author = "Eric Holscher, Chris Adams",
+    author_email = "eric@ericholscher.com",
+    description = "A crawler using the Django Test Client",
+    url = "http://github.com/ericholscher/django-crawler/tree/master",
+)
